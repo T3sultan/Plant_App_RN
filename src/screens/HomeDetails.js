95877ae728729426dc98e27e50218e7a40bece77
“T3sultan” <incognito.soul011@gmail.com>
2022-05-21 01:04:15 +0600

    folder structure

---
+++ src/screens/HomeDetails.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const HomeDetails = () => {
+  return (
+    <View>
+      <Text>HomeDetails</Text>
+    </View>
+  );
+};
+
+export default HomeDetails;
+
+const styles = StyleSheet.create({});
